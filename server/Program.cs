88e974f2eb00afb672c1cd08127a2f39b3566705
shooooooma415 <shoma20040415@icloud.com>
2025-06-20 18:41:19 +0900

    hotfix: Program.csの中身を修正

--- server/Program.cs
+++ server/Program.cs
@@ -4,30 +4,38 @@ using TodoApp.Domain.Interfaces;
 using TodoApp.Infrastructure.Data;
 using TodoApp.Infrastructure.Repositories;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace TodoApp.WebAPI;
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<TodoDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddScoped<ITodoRepository, TodoRepository>();
-builder.Services.AddScoped<GetAllTodosQueryHandler>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<TodoDbContext>(options =>
+            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddScoped<ITodoRepository, TodoRepository>();
+        builder.Services.AddScoped<GetAllTodosQueryHandler>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
